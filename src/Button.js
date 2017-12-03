7114600491f0a33ad8ad2334f56bbc3dc152db40
ninayar <nn1024@nyu.edu>
2017-12-03 02:55:44 -0500

    movies

--- src/Button.js
+++ src/Button.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import MoviePedia from './MoviePedia';
 import Chat from './Chat';
-import { FloatingMenu, MainButton, ChildButton } from 'react-floating-button-menu';
-import MdAdd from 'react-icons/lib/md/add';
-import MdClose from 'react-icons/lib/md/close';
+
 class Button extends Component {
 
   render () {
