d4d98603e0a9566b56070b904cfd4c840345ef28
ninayar <nn1024@nyu.edu>
2017-12-02 21:30:20 -0500

    button_float

--- src/Button.js
+++ src/Button.js
@@ -6,59 +6,9 @@ import MdAdd from 'react-icons/lib/md/add';
 import MdClose from 'react-icons/lib/md/close';
 class Button extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = { modalActive: false }
-  }
-
-  openModal ()  {
-    this.setState({ modalActive: true })
-  }
-
-  closeModal  () {
-    this.setState({ modalActive: false })
-  }
-
   render () {
     return (
-  //     <div>
-  //   <FloatingMenu slideSpeed={500} direction="left">
-  //   <MainButton
-  //     iconResting={MdAdd}
-  //     iconActive={MdClose}
-  //     iconColor="white"
-  //     backgroundColor="black"
-  //     buttonSize="56"
-  //     onClick={this.openModal.bind(this)}
-  //   />
-  // </FloatingMenu>
-  //   {this.state.modalActive ?
-  //     <div className='modalDialog'>
-  //       <a title='Close' onClick={this.closeModal.bind(this)}>X</a>
-  //       <ExampleChat/>
-  //     </div>
-  //     :null
-  //   }
-  //   </div>
-  // <a href="#top-section" className="btn-floating btn-large red" onClick={this.openModal.bind(this)}>
-  //     <i className="fa fa-arrow-up"></i>
-  // </a>
-
-
-      <div>
-      <div className="Collapse" id="ChatBox">
-      {this.state.modalActive ?
-        <div className='modalDialog'>
-          <a title='Close' onClick={this.closeModal.bind(this)}>X Close</a>
-          <Chat/>
-        </div>
-        :null
-      }
-      </div>
-      <button className="btn btn-primary" href="#ChatBox" type="button" onClick={this.openModal.bind(this)} data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-      ChatNow
-      </button>
-      </div>
+      <div><Chat/></div>
     )
   }
 }
--- src/Chat.js
+++ src/Chat.js
@@ -1,25 +1,116 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ChatBot,{ Loading } from 'react-simple-chatbot';
-import MoviePedia from './MoviePedia'
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
+
+class Summary extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      movieName: '',
+      customerName: '',
+      zip: '',
+      result: 'error'
+
+    };
+  }
+
+  componentWillMount() {
+    const { steps } = this.props;
+    const { movieName,customerName,zip,result} = steps;
+
+    const search = movieName.value;
+    console.log(search);
+    const url = `http://www.omdbapi.com/?apikey=cc75c508&s=${search}`;
+    console.log(url);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4) {
+      const data = JSON.parse(this.responseText);
+      console.log(data);
+    }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+    this.setState({ movieName,customerName,zip,result});
+  }
+
+  render(){
+    const {movieName,customerName,zip,result} = this.state;
+
+    return(
+      <div style = {{width: '100%'}}>
+        <p> Details: <br/> {customerName.value} , {zip.value}</p>
+        <h4>Movie Deatils</h4>
+        <p>{result.value}</p>
+      </div>
+    );
+  }
+}
+
+Summary.propTypes = {
+  steps: PropTypes.object,
+};
+
+Summary.defaultProps = {
+  steps: undefined,
+};
+
+class NearBy extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: ''
+
+    };
+  }
+
+  componentWillMount() {
+    const { steps } = this.props;
+    const { zip } = steps;
+    this.setState({zip});
+  }
+
+  render(){
+    const {zip} = this.state;
+    return(
+      <div style = {{width: '100%'}}>
+        <p> Details: {zip.value}</p>
+      </div>
+    );
+  }
+}
+
+NearBy.propTypes = {
+  steps: PropTypes.object,
+};
+
+NearBy.defaultProps = {
+  steps: undefined,
+};
+
+
 class Chat extends Component {
 
   render(){
     return (
 
-      <ChatBot steps = {[
+      <ChatBot
+      floating	= {true}
+      steps = {[
         {
           id: '0',
-          message: 'Welcome to MoviezMadeEasy!',
+          message: 'Welcome to MovieTix!',
           trigger: '1',
         },
         {
           id: '1',
           message: 'What is your name?',
-          trigger: '2',
+          trigger: 'customerName',
           },
           {
-            id: '2',
+            id: 'customerName',
             user: true,
             trigger: '3',
           },
@@ -30,90 +121,83 @@ class Chat extends Component {
           },
           {
             id: '4',
-            message: 'What can I do for you?',
+            message: 'What is your zip code?',
+            trigger: 'zip',
+          },
+          {
+            id: 'zip',
+            user: true,
             trigger: '5',
           },
           {
-         id: '5',
+            id: '5',
+            message: 'What can I do for you?',
+            trigger: '6',
+          },
+          {
+         id: '6',
          options: [
            { value: 1, label: 'Buy Movie Tickets', trigger: '7' },
-           { value: 2, label: 'Look up movies near by', trigger: '6' },
+           { value: 2, label: 'Look up movies near by', trigger: 'Near1' },
            { value: 3, label: 'Watch movie trailers', trigger: '6' },
          ],
        },
+
        {
-         id: '6',
-         message: 'Wrong answer, try again.',
-         trigger: '5',
+         id: 'Near1',
+         component: <NearBy/>,
+         trigger: '6',
        },
        {
          id: '7',
          message: 'Awesome! Lets start',
-         trigger: '8',
-       },
-       {
-         id: '8',
-         message: 'In which city would you like to buy ticket in?',
          trigger: '9',
        },
        {
          id: '9',
-         user: true,
+         message: 'Do you know what movie you want to buy tickets for?',
          trigger: '10',
        },
        {
-         id: '10',
-         message: 'Lets buy tickets in {previousValue}',
-         trigger: '11',
-       },
-       {
-         id: '11',
-         message: 'Do you know what movie you want to buy tickets for?',
-         trigger: '12',
-       },
-       {
-      id: '12',
+      id: '10',
       options: [
-        { value: 1, label: 'Yes', trigger: '14' },
-        { value: 2, label: 'No', trigger: '13' },
+        { value: 1, label: 'Yes', trigger: '12' },
+        { value: 2, label: 'No', trigger: '11' },
       ],
       },
       {
-        id: '13',
+        id: '11',
         message: 'Not supported yet!! :(',
-        trigger: '12',
+        trigger: '9',
       },
       {
-        id: '14',
+        id: '12',
         message: 'What is the movie you want to buy ticket for?',
-        trigger: '15',
+        trigger: 'movieName',
       },
       {
-        id: '15',
+        id: 'movieName',
         user: true,
-        trigger: '16',
+        trigger: '13',
       },
       {
-        id: '16',
-          message: 'Awesome! Lets get you those tickets!',
-        trigger: '17',
-      },
-      {
-        id: '17',
-        message: 'Confirm the movie name again',
-        trigger: 'search',
-      },
-      {
-        id: 'search',
-        user: true,
-        trigger: '18',
-      },
-      {
-        id: '18',
-        component: <MoviePedia />,
+        id: '13',
+        component: <Summary />,
         waitAction: true,
-        trigger: '17',
+        trigger: '19',
       },
+      {
+        id: '19',
+        message: 'Is this what you wanted?',
+        trigger: '20',
+      },
+      {
+     id: '20',
+     options: [
+       { value: 1, label: 'Yes', trigger: '13' },
+       { value: 2, label: 'No', trigger: '12' },
+     ],
+     },
       ]}
       />
 
