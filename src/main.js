6e41bbd7a81f97bb466c374e9827babe22e8f4d7
shwetakilledar <shwetakilledar25@gmail.com>
2017-12-03 19:53:03 -0500

    Updated code structure

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Chat from './Chat';
-import Button from './Button';
-import Nav from './Nav';
-import Header from './Header';
-import Movie from './Movie';
-import Contact from './Contact';
-import Footer from './Footer';
-import Event from './Event';
+import Chat from './component/Chat.js';
+import Button from './component/Button.js';
+import Nav from './component/Nav.js';
+import Header from './component/Header.js';
+import Movie from './component/Movie.js';
+import Contact from './component/Contact.js';
+import Footer from './component/Footer.js';
+import Event from './component/Event.js';
 
 class App extends React.Component {
   render () {
     return(
       <div>
-        <Nav/>
-        <Header/>
-        <Movie/>
-        <Event/>
+          <Nav/>
+          <Header/>
+          <Movie/>
+          <Event/>
           <Contact/>
           <Footer/>
-          </div>
+        </div>
         )
   }
 }
